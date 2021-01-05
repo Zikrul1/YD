0bd5031cf0aff23ba7c82800e8ececb189d834cc
Zikrul1 <35360469+Zikrul1@users.noreply.github.com>
2021-01-05 23:35:54 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+termux-setup-storage ;apt update && apt install curl && curl https://raw.githubusercontent.com/SarfarazRLZ/Termux-For-Youtube/master/youtube_settings.sh -o youtube.sh && chmod +x youtube.sh && dos2unix youtube.sh && ./youtube.sh
