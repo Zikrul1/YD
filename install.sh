ab14c93451c97206909b70870222f1b7ce033bfc
Zikrul1 <35360469+Zikrul1@users.noreply.github.com>
2021-01-05 23:44:30 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,32 @@
-termux-setup-storage ;apt update && apt install curl && curl https://raw.githubusercontent.com/SarfarazRLZ/Termux-For-Youtube/master/youtube_settings.sh -o youtube.sh && chmod +x youtube.sh && dos2unix youtube.sh && ./youtube.sh
+
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Updating default packages"
+apt update && apt -y upgrade
+echo "Installing Required Tools"
+apt install termux-api figlet python ffmpeg -y
+
+echo "Requesting access to storage"
+termux-setup-storage
+sleep 5
+
+echo "Installing Dependencies"
+yes | pip install youtube-dl
+
+echo "Creating the Youtube folder to download the files"
+mkdir ~/storage/shared/youtube
+
+echo "Creating youtube-dl folder for config"
+mkdir -p ~/.config/youtube-dl
+
+echo "Creating bin folder"
+mkdir ~/bin
+ 
+echo "Downloading and installing termux-url-opener"
+curl https://raw.githubusercontent.com/SarfarazRLZ/Termux-For-Youtube/master/termux-url-opener -o ~/bin/termux-url-opener
+sleep 2
+dos2unix ~/bin/termux-url-opener
+
+echo ""
+echo "Modified by :"
+figlet RAJ
